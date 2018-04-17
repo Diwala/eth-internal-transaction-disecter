b1ea304aaa30de46df45a20fbfcd3e21be414aaf
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2018-04-17 09:44:29 +0200

    removed build from git

--- hosted-parser/src/App.js
+++ hosted-parser/src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
           <div className="txn">{output(this.state.transaction)}</div>
         </div>
         <footer>
-          Made with 💜 by <a href="https://diwala.io/" target="_blank">https://diwala.io/</a>
+          Made with <span role="img">💜</span> by <a href="https://diwala.io/" target="_blank" rel="noopener noreferrer">https://diwala.io/</a>
         </footer>
       </div>
     );
